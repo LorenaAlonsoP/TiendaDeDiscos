52c91aaf18d370975586006bc4657d8c98f70de2
Lorena Alonso <lorenaalonsop26@gmail.com>
2018-01-24 11:51:46 +0100

    Añadida clase Discos y Tienda de Discos

---
+++ TiendaDeDiscos.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Discos here.
+ *
+ * @author (Lorena Alonso Pedreira)
+ */
+public class TiendaDeDiscos
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Discos> discosDeMusica;
+
+    /**
+     * Constructor for objects of class TiendaDeDiscos
+     */
+    public TiendaDeDiscos()
+    {
+        discosDeMusica = new ArrayList<Discos>();
+    }
+
+}
